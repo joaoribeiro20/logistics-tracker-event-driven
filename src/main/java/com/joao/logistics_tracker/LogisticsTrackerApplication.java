e66db9a212552e6a1bdd34fcea0abc0a292a042d
joaoribeiro20 <jvalves217@gmail.com>
2026-05-18 19:39:06 -0300

    first commit

---
+++ src/main/java/com/joao/logistics_tracker/LogisticsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.joao.logistics_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joao/logistics_tracker/LogisticsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao.logistics_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
