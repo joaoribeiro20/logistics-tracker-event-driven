b3845f4caa72e4280c0966b4b0c3cd1a9042ab51
joaoribeiro20 <jvalves217@gmail.com>
2026-05-22 19:11:55 -0300

    chore: configure initial project setup and infrastructure

--- src/main/java/com/joao/logistics_tracker/LogisticsTrackerApplication.java
+++ src/main/java/com/joao/logistics_tracker/LogisticsTrackerApplication.java
@@ -2,7 +2,9 @@ package com.joao.logistics_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class LogisticsTrackerApplication {
 
