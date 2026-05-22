01b4de62aa822d0c18fe90225fe52ea9ef0075ca
joaoribeiro20 <jvalves217@gmail.com>
2026-05-22 19:16:54 -0300

    feat: add enum status tracking

---
+++ src/main/java/com/joao/logistics_tracker/domain/enums/TrackingStatus.java
@@ -0,0 +1,10 @@
+package com.joao.logistics_tracker.domain.enums;
+
+public enum TrackingStatus {
+    DISPATCHED,
+    AT_DISTRIBUTOR,
+    COLLECTED,
+    OUT_FOR_DELIVERY,
+    DELIVERED,
+    FAILED
+}
